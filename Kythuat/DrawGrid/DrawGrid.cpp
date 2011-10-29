c10c3fa534a66fff982ed40e99d4ceacf0103fec
phanmanhdan@gmail.com <phanmanhdan@gmail.com@0e6a1fab-816b-e59c-48b9-b7008c7d48ea>
2011-10-29 10:53:04 +0000

    Tổ chức lai cấu trúc thư mục 2

---
+++ Kythuat/DrawGrid/DrawGrid.cpp
@@ -0,0 +1,29 @@
+﻿
+void DrawGrid(int gridSize = 10, float gridElev=0, float lineWidth = 0.5f)
+{
+	//	Set up some nice attributes for drawing the grid.
+	glPushAttrib(GL_LINE_BIT | GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT);
+	glEnable(GL_LINE_SMOOTH);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDisable(GL_LIGHTING);
+	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glLineWidth(lineWidth);
+
+	//	Create the grid.
+
+    glBegin(GL_LINES);
+	{
+		for (int i = -gridSize; i <= gridSize; i++)
+		{
+			glColor4f(0.2f, 0.2f, 0.2f, 0.8f);
+			glVertex3f((float)i, 0, -(float)gridSize);
+			glVertex3f((float)i, 0, +(float)gridSize);
+			glVertex3f(-(float)gridSize, 0, (float)i);
+			glVertex3f(+(float)gridSize, 0, (float)i);
+		}
+	}
+	glEnd();
+
+	glPopAttrib();
+}
